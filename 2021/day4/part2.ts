249cbf6dab871458e1cbdfb194a73c2479087d3c
Spikatrix <12792882+Spikatrix@users.noreply.github.com>
2021-12-04 12:10:31 +0530

    Day 4

---
+++ 2021/day4/part2.ts
@@ -0,0 +1,70 @@
+import fs from "fs";
+
+const fileContent: string = fs.readFileSync('input', 'utf8');
+const lines: string[] = fileContent.split("\n").filter(line => line.trim());
+
+const inputs: number[] = lines[0].split(",").map(inputStr => parseInt(inputStr));
+const boardSize = 5;
+
+type BoardType = Array<number[]>;
+const boards: BoardType[] = [];
+
+// Get all board inputs
+let lineIndex: number = 1;
+while (lineIndex < lines.length) {
+	const currentBoardIndex = boards.length;
+	boards.push([]);
+	let boardLineIndex = 0;
+	while (boardLineIndex < boardSize) {
+		const boardLineInts: number[] = lines[lineIndex].split(" ")
+											.filter(value => value.trim())
+											.map(valueStr => parseInt(valueStr));
+		boards[currentBoardIndex].push(boardLineInts);
+		boardLineIndex++;
+		lineIndex++;
+	}
+}
+
+let currentInputIndex: number = 4;
+let wonBoard: number = -1;
+while (true) {
+	while (true) {
+		const currrentInputs = inputs.slice(0, currentInputIndex);
+
+		boards.some((board, boardIndex) => {
+			const transpose: BoardType = board[0].map((_, colIndex) => board.map(row => row[colIndex]));
+
+			const checkRow = (row: number[]) => row.every(value => currrentInputs.includes(value));
+			if (board.some(checkRow) || transpose.some(checkRow)) {
+				wonBoard = boardIndex;
+			}
+
+			return wonBoard !== -1;
+		});
+
+		if (wonBoard === -1) {
+			currentInputIndex++;
+		} else {
+			break;
+		}
+	}
+
+	if (boards.length > 1) {
+		boards.splice(wonBoard, 1);
+		wonBoard = -1;
+	} else {
+		break;
+	}
+}
+
+const finalInputs = inputs.slice(0, currentInputIndex);
+let unmarkedSum = 0;
+boards[wonBoard].forEach(row => {
+	row.forEach(value => {
+		if (!finalInputs.includes(value)) {
+			unmarkedSum += value;
+		}
+	});
+});
+
+console.log(unmarkedSum * finalInputs[finalInputs.length - 1]);
