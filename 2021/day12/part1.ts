b053d1a65300603cdba89f129f1d32eec6941455
Spikatrix <12792882+Spikatrix@users.noreply.github.com>
2021-12-12 15:48:55 +0530

    Day 12

---
+++ 2021/day12/part1.ts
@@ -0,0 +1,57 @@
+import fs from "fs";
+
+const fileContent: string = fs.readFileSync('input', 'utf8');
+const lines: string[] = fileContent.split("\n").filter(line => line.trim());
+
+const adjacencyMatrix: number[][] = []
+const vertices: string[] = [];
+lines.forEach(line => {
+	const [src, dest] = line.split('-');
+	if (!vertices.includes(src)) {
+		vertices.push(src);
+	}
+	if (!vertices.includes(dest)) {
+		vertices.push(dest);
+	}
+});
+
+function printAdjacencyMatrix(matrix: number[][], vertices: string[]) {
+	const axis = vertices.map(vertex => vertex.charAt(0))
+	console.log('   ', axis.join('  '));
+	matrix.forEach((row, index) => {
+		console.log(axis[index], row)
+	})
+}
+
+vertices.forEach(() => adjacencyMatrix.push(new Array(vertices.length).fill(0) as number[]));
+
+lines.forEach(line => {
+	const [srcIndex, destIndex] = line.split('-').map(vertex => vertices.indexOf(vertex));
+	adjacencyMatrix[srcIndex][destIndex]++;
+	adjacencyMatrix[destIndex][srcIndex]++;
+});
+
+let pathCount: number = 0;
+
+function traverse(currentIndex: number, visitedVertices: string[] = []) {
+	const vertex = vertices[currentIndex];
+	if (/^[a-z]*$/.test(vertex)) {
+		visitedVertices.push(vertex);
+	}
+	if (vertex === 'end') {
+		pathCount++;
+		return;
+	}
+
+	adjacencyMatrix[currentIndex].forEach((cell, cellIndex) => {
+		if (cell && !visitedVertices.includes(vertices[cellIndex])) {
+			traverse(cellIndex, [ ...visitedVertices ]);
+		}
+	});
+}
+
+// printAdjacencyMatrix(adjacencyMatrix, vertices);
+
+traverse(vertices.indexOf('start'));
+console.log(pathCount);
+
