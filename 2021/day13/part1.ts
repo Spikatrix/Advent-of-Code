6ea787e0e12e4b3b0755149dc1aca9bb9bc155f2
Spikatrix <12792882+Spikatrix@users.noreply.github.com>
2021-12-13 21:50:03 +0530

    Day 13

---
+++ 2021/day13/part1.ts
@@ -0,0 +1,46 @@
+import fs from "fs";
+
+const fileContent: string = fs.readFileSync('input', 'utf8');
+const lines: string[] = fileContent.split("\n").filter(line => line.trim());
+
+type Direction = 'x' | 'y'
+type Point = { x: number, y: number };
+type Fold = { direction: Direction, location: number };
+let dots: Point[] = [];
+const folds: Fold[] = [];
+
+lines.forEach(line => {
+	if (line.includes(',')) {
+		const [ x, y ] = line.split(',').map(coordStr => parseInt(coordStr));
+		dots.push({ x, y });
+	} else if (line.includes('=')) {
+		const [ directionStr, locationStr ] = line.substring(11).split('=');
+		const direction = directionStr as Direction;
+		const location = parseInt(locationStr);
+		folds.push({ direction, location })
+	}
+});
+
+folds.forEach((fold, index) => {
+	if (index !== 0) {
+		return;
+	}
+
+	dots = dots.filter(dot => dot[fold.direction] !== fold.location);
+	while (true) {
+		const dotIndex = dots.findIndex(dot => dot[fold.direction] > fold.location);
+		if (dotIndex === -1) {
+			break;
+		}
+
+
+		const newDot = { ...dots[dotIndex] };
+		dots.splice(dotIndex, 1);
+		newDot[fold.direction] = 2 * fold.location - newDot[fold.direction];
+		if (!dots.find(dot => dot.x === newDot.x && dot.y === newDot.y)) {
+			dots.push(newDot);
+		}
+	}
+});
+
+console.log(dots.length);
